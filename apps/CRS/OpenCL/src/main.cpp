774973df11bab1bab2606f1d71627645922fa896
Ricardo Menotti <menotti@gmail.com>
2020-06-18 14:03:43 -0300

    Getting new results

--- apps/CRS/OpenCL/src/main.cpp
+++ apps/CRS/OpenCL/src/main.cpp
@@ -339,6 +339,7 @@ int main(int argc, const char** argv) {
   stats += ": Kernel Execution Time: " + std::to_string(kernel_total_exec_time);
   stats += ": Kernel Giga-Semblances-Trace/s: " + std::to_string(kernel_stps);
   LOG(INFO, stats);
+  std::cout << (int)(total_exec_time*1000) << std::endl;
 
   // Delinearizes data and save it into a *.su file
   for(int i=0; i < ncdps; i++) {
